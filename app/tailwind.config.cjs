2c88328fdef16b62889db78b001b8490995b6180
OriginalByteMe <noahrijkaard@gmail.com>
2024-03-10 18:25:08 +0800

    Add meteor affect to about page

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -49,6 +49,21 @@ module.exports = {
 		letterSpacing: {
 			wide: '.025em',
 		},
+		extend: {
+      animation: {
+        "meteor-effect": "meteor 5s linear infinite",
+      },
+      keyframes: {
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
+          },
+        },
+      },
+    },
 	},
 	plugins: [
 		plugin(function ({ addBase, theme }) {
